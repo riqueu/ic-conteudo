f7e875ca18351009541d7a6c347a66c80434d7ae
Henrique <riquecbeltrao@hotmail.com>
2024-05-13 08:58:32 -0300

    atualizando elevador

--- segundo_bimestre/projeto_elevador/main.py
+++ segundo_bimestre/projeto_elevador/main.py
@@ -1,91 +1,110 @@
 import random
-
+from collections import defaultdict
 
 LOTACAO = 8
-# Elevadores: Programação Estruturada (Tentar fazer com orientação a objetos depois)
+ANDARES = 14
+POPULACAO = defaultdict(lambda: 0)
+FILAS_DE_ANDAR = defaultdict(lambda: 0)
 
-"""
-andar_elevador: int
-capacidade: int
-passageiros: int
-direção: [-1, 0, 1]
-chamadas: list/set
-"""
-
-# Condições Iniciais:
-ci_elv = [('andar', 0), ('n_passageiros', 0), ('direção', 0), ('chamadas', [])]
+# variáveis de estado
+ci_elv = [('andar', 0), ('npass', 0), ('passageiros', []), ('direção', 0), ('chamadas', [])]
 ci_sim = [('fila', 0), ('energia', 0.0), ('viagens', 0)]
 
 
-# API ELEVADOR
-
+# API do elevador
 def embarque(andar=0):
-    pos_elevadores = [e['andar'] for e in elvs]
-    if andar in pos_elevadores:
-        elv_i = pos_elevadores.index(andar)  # descobre o elevador está no andar
-        elv = elvs[elv_i]
-        n_passageiros = elv[elv]['n_passageiros']
-        while n_passageiros < LOTACAO:
-            n_passageiros += 1
+    pos_elv = [e['andar'] for e in elevadores]
+    if andar in pos_elv:
+        elv_i = pos_elv.index(andar)  # descobre qual elevador está no andar
+        elv = elevadores[elv_i]
+        npass = elv['npass']
+        while npass < LOTACAO:
+            npass += 1
             sim['fila'] -= 1
             if sim['fila'] == 0:
                 break
-        elv['n_passageiros'] = n_passageiros
+        elv['npass'] = npass
 
 
-def desembarque(n: int):
+def operacao():
+    """
+    Define as ações do elevador em função da simulação e dos elevadores
+    """
+    for elvs in elevadores:
+        pass
+
+
+def desembarque(n):
     pass
 
 
-def chamada(n: int):
+def chamada(n):
+    """
+    Atualiza estado em resposta a uma chamada no andar n
+    :param n: andar em que foi chamado o elevador
+    :type n: int
+    """
     pass
 
 
-def input_int(n: int):
+def input_int(n):
     pass
 
 
-# Eventos:
+# Eventos
 eventos = {
     'embarque': embarque,
-    'desembarque': desembarque,
     'chamada': chamada,
     'input_int': input_int,
+    'desembarque': desembarque
 }
 
 
 def init_sim():
-    # Elevadores:
+    # Elevadores
     estado_e1 = dict(ci_elv)
     estado_e2 = dict(ci_elv)
-    # Simulação:
+    # simulação
     estado_sim = dict(ci_sim)
+    for i in range(1, ANDARES + 1):
+        POPULACAO[i]
     return (estado_e1, estado_e2), estado_sim
 
 
 def gera_fila():
-    """ Retorna um int para acrescentar à fila no loop de evento """
     n = random.randint(1, 5)
     sim['fila'].append(n)
-    if 0 not in elvs[0]['chamadas']:
-        elvs[0]['chamadas'].append(0)
-    if 0 not in elvs[1]['chamadas']:
-        elvs[1]['chamadas'].append(0)
+    atualiza_chamadas()
 
 
-def mover_elevador(elevador):
+def atualiza_chamadas(andar=0):
+    if andar not in elevadores[0]['chamadas']:
+        elevadores[0]['chamadas'].append(andar)
+    if andar not in elevadores[1]['chamadas']:
+        elevadores[1]['chamadas'].append(andar)
+
+
+def move(elv):
     pass
+
+
+def saidas():
+    andar = random.randint(1, ANDARES)
+    descendo = random.randint(0, POPULACAO[andar])
+    FILAS_DE_ANDAR[andar] += descendo
+    atualiza_chamadas(andar)
 
 
 def loop_de_evento(n):
     i = 0
     while True:
         gera_fila()
-        if sim['fila']:
-            eventos['embarque']()
-            #  mover_elevador()
+        operacao()
+        saidas()
+        if i > n:
+            break
 
 
 if __name__ == "__main__":
-    elvs, sim = init_sim()
+    elevadores, sim = init_sim()
     loop_de_evento(10)
